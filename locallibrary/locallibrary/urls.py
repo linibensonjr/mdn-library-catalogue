2bd8dfa489deea7a58d2ff91118739d09079ff66
Iniobong Benson <linibensonjr@gmail.com>
2022-02-27 14:02:25 +0100

    update model

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+ 

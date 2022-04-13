b35aeb29f4786c816b8fc0d155a6220c7bb60a3d
Iniobong Benson <linibensonjr@gmail.com>
2022-04-13 13:45:56 +0100

    customize admin site

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -39,6 +39,13 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
